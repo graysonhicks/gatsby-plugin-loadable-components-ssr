515d7da8e08cd16b1d045926827b69f64ddd4021
Hector del Rio <hector.del.rio@futurice.com>
2020-03-03 11:44:32 +0200

    Fixed a console log error on develop stages

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,8 +1,11 @@
-import { hydrate } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import { loadableReady } from '@loadable/component';
 
 export const replaceHydrateFunction = () => (element, container, callback) => {
   loadableReady(() => {
-    hydrate(element, container, callback);
+    // Using ReactDOM.hydrate on develop will throw an error in console
+    const renderFn = process.env.GATSBY_BUILD_STAGE.includes('develop') ? render : hydrate;
+
+    renderFn(element, container, callback);
   });
 };
