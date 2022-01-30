fa790221dba96eed706ba39daa1c42fa6f5abcf1
Michal Piechowiak <misiek.piechowiak@gmail.com>
2022-01-30 11:11:54 +0100

    use dummy stats for develop

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from "path"
 
 import LoadablePlugin from "@loadable/webpack-plugin"
 
@@ -7,21 +7,33 @@ import { LOADABLE_STATS_FILE_PATH } from "./constant"
 const isWin = process.platform === "win32"
 
 export const onCreateWebpackConfig = ({ actions, stage }) => {
-  if (
-    stage === "build-javascript" ||
-    stage === "develop" ||
-    stage === "develop-html"
-  ) {
-    actions.setWebpackConfig({
-      plugins: [
-        new LoadablePlugin({
-          filename: path.join(process.cwd(), LOADABLE_STATS_FILE_PATH),
-          writeToDisk: true,
-          outputAsset: !isWin,
-        }),
-      ],
-    })
+  // for builds we point to stats file that will be produced by `build-javascript`
+  // for develop we point to empty mocked stats, just so gatsby-ssr continue to work
+
+  const fullPath =
+    stage === `build-javascript` || stage === `build-html`
+      ? path.join(process.cwd(), LOADABLE_STATS_FILE_PATH)
+      : require.resolve(`./develop-stats.json`)
+
+  const config = {
+    resolve: {
+      alias: {
+        "gatsby-plugin-loadable-components-ssr-stats-file": fullPath,
+      },
+    },
   }
+
+  if (stage === "build-javascript") {
+    config.plugins = [
+      new LoadablePlugin({
+        filename: fullPath,
+        writeToDisk: true,
+        outputAsset: !isWin,
+      }),
+    ]
+  }
+
+  actions.setWebpackConfig(config)
 }
 
 export const onCreateBabelConfig = ({ actions }) => {
