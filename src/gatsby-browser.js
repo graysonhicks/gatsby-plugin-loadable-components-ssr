a649d975a4fbb38efe3a3e930796004998165be5
Grayson Hicks <graysonhicks@gmail.com>
2022-09-16 10:58:38 -0500

    react 18 render logic

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,18 +1,27 @@
-import { render, hydrate } from "react-dom"
 import { loadableReady } from "@loadable/component"
+import { major } from "semver"
+
+const react18 = JSON.stringify(
+  major(require(`react-dom/package.json`).version) >= 18
+)
 
 export const replaceHydrateFunction =
   (_, options) => (element, container, callback) => {
+    if (react18) {
+      const reactDomClient = require(`react-dom/client`)
+    } else {
+      const reactDomClient = require(`react-dom`)
+    }
     loadableReady(() => {
       const renderFn =
         typeof options.useHydrate === "undefined"
           ? // Using ReactDOM.hydrate on develop will throw an error in console
             process.env.BUILD_STAGE.includes("develop")
-            ? render
-            : hydrate
+            ? reactDomClient.render
+            : reactDomClient.hydrate
           : !!options.useHydrate
-          ? hydrate
-          : render
+          ? reactDomClient.hydrate
+          : reactDomClient.render
 
       renderFn(element, container, callback)
     })
